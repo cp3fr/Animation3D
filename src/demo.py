23e4c6642170c8dad497d84c410acff9769cd26f
cp3fr <pfeifferchr@gmail.com>
2020-09-16 15:53:41 +0200

    updated readme

--- src/demo.py
+++ src/demo.py
@@ -1,6 +1,9 @@
 from Animation3D import *
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('../data/data.csv')
+
+data = data.loc[:, ('ts', 'PositionX', 'PositionY', 'PositionZ', \
+                    'rot_x_quat', 'rot_y_quat', 'rot_z_quat', 'rot_w_quat')]
 
 anim = Animation3D(data, speed=1., fps=25, view=(45, -90))
 
